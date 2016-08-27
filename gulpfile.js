5dc6f1c7fd51c08c02b7ff58fccb30afbe1d33c3
Matthew Townsen <matthewtownsen@gmail.com>
2016-08-27 08:20:57 -0600

    install bootstrap. update grunt and file structure to promote drag and drop features.

--- gulpfile.js
+++ gulpfile.js
@@ -6,24 +6,33 @@ var config = {
 		app: './app/',
 		index: './app/index.html',
 	    jsPaths: [
-	    	'./app/app.js',
-	    	'./app/view1/*.js',
-	    	'./app/view2/*.js',
-	    	'!./app/*/*_test.js',
-	    	'./app/components/version/*.js',
-	    	'!./app/components/version/*_test.js',
+			'./app/app.js',
+			'./app/components/*/*.js',
+			'!./app/components/*/*_test.js',
+	    ],
+	    cssPaths: [
+	    	'./app/app.css',
 	    ],
 	    injectOptions: {
 		     addRootSlash: false,
 		     ignorePath: 'app/'
 		},
-		wiredepOptions: {
+		bowerDir: {
 			directory: './app/bower_components'
 		}
 };
  
+
+gulp.task('styles', function () {
+  var target = gulp.src(config.index);
+  // It's not necessary to read the files (will speed up things), we're only after their paths: 
+  var sources = gulp.src(config.cssPaths, {read: false});
+
+  return target.pipe(inject(sources, config.injectOptions))
+    .pipe(gulp.dest(config.app));
+}); 
  
-gulp.task('index', function () {
+gulp.task('scripts', function () {
   var target = gulp.src(config.index);
   // It's not necessary to read the files (will speed up things), we're only after their paths: 
   var sources = gulp.src(config.jsPaths, {read: false});
@@ -35,10 +44,7 @@ gulp.task('index', function () {
 
 gulp.task('bower', function () {
   var target = gulp.src(config.index);
-  // It's not necessary to read the files (will speed up things), we're only after their paths: 
-  var sources = gulp.src(config.jsPaths, {read: false});
- 
-  return target.pipe(wiredep(config.wiredepOptions))
+  return target.pipe(wiredep(config.bowerDir))
     .pipe(gulp.dest(config.app));
 });
 
